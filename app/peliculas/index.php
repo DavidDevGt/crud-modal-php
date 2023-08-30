747fe34d5524f9147599915fd0b722e6c0a26cf8
Davidvl <josuedavidvl18@gmail.com>
2023-08-29 22:04:35 -0600

    formulario del modal 1

--- app/peliculas/index.php
+++ app/peliculas/index.php
@@ -1,3 +1,9 @@
+<?php
+
+require '../config/database.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,10 +17,36 @@
 
 
 
+    <div class="container py-3"> 
+        <h3 class="text-center mt-4">Peliculas</h3>
 
+        <div class="row justify-content-end">
+            <div class="col-auto">
+                <a href="#" data-bs-toggle="modal" data-bs-target="#nuevoModal" class="btn btn-primary p-2"><i class="fa-solid fa-circle-plus"></i>Nuevo registro</a>
+            </div>
+        </div>
 
+        <table class="table table-sm table-striped table-hover mt-4">
+            <thead class="table-dark">
+                <tr>
+                    <th>#</th>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>Género</th>
+                    <th>Acción</th>
+                </tr>
+            </thead>
+            <tbody>
 
+            </tbody>
+        </table>
+    </div>
 
+    <?php
+        $sqlGenero = "SELECT id, nombre FROM genero";
+        $generos = $conn->query($sqlGenero);
+    ?>
+    <?php include 'nuevoModal.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
